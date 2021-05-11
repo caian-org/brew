edcff48aa5b22578092a7264e6b0e8092d762313
Caian R. Ertl <hi@caian.org>
2021-05-10 23:20:07 -0300

    fix: sshpass url, remove formula require, fix style (quotes)

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class Sshpass < Formula
-  url 'https://github.com/caian-org/brew/raw/master/Packages/sshpass-1.06.tar.gz'
-  homepage 'https://sourceforge.net/projects/sshpass'
-  sha256 'c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60'
+  url "https://github.com/caian-org/homebrew-formulas/raw/master/Packages/sshpass-1.06.tar.gz"
+  homepage "https://sourceforge.net/projects/sshpass"
+  sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
 
-  depends_on 'gnu-sed'
+  depends_on "gnu-sed"
 
   def install
     system "./configure", "--disable-debug",
--- Formula/steghide.rb
+++ Formula/steghide.rb
@@ -1,23 +1,21 @@
-require 'formula'
-
 class Steghide < Formula
-  url 'https://github.com/caian-org/steghide/archive/0.5.1.tar.gz'
-  homepage 'https://github.com/caian-org/steghide'
-  sha256 '8ece8b956c7c509534b5ca7f11189efb49bfc6fb8924a933387c0fcd64894bef'
+  url "https://github.com/caian-org/steghide/archive/0.5.1.tar.gz"
+  homepage "https://github.com/caian-org/steghide"
+  sha256 "8ece8b956c7c509534b5ca7f11189efb49bfc6fb8924a933387c0fcd64894bef"
 
-  depends_on 'gettext'
-  depends_on 'mhash'
-  depends_on 'libtool'
+  depends_on "gettext"
+  depends_on "mhash"
+  depends_on "libtool"
 
   def install
     system "LDFLAGS=-L/usr/local/opt/gettext/lib CPPFLAGS=-I/usr/local/opt/gettext/include ./configure --prefix=#{prefix}"
 
     system 'sed -i \'\' -e "s/SHELL =.*/SHELL = \/bin\/bash/g" src/Makefile'
     system 'sed -i \'\' -e "s/LIBTOOL =.*/LIBTOOL = glibtool --tag CXX/g" src/Makefile'
-    system 'cd src && make && make install'
+    system "cd src && make && make install"
   end
 
   def test
-    system 'steghide'
+    system "steghide"
   end
 end
