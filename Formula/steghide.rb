daa2e4b8ec177d5ec15f1dae8edce5171677c1d8
Caian Ertl <hi@caian.org>
2022-01-26 20:28:22 -0300

    fix(formula/steghide): url and homepage

--- Formula/steghide.rb
+++ Formula/steghide.rb
@@ -1,6 +1,6 @@
 class Steghide < Formula
-  url "https://github.com/caian-org/steghide/archive/0.5.1.tar.gz"
-  homepage "https://github.com/caian-org/steghide"
+  url "https://github.com/e-graveyard/steghide/archive/0.5.1.tar.gz"
+  homepage "https://github.com/e-graveyard/steghide"
   sha256 "8ece8b956c7c509534b5ca7f11189efb49bfc6fb8924a933387c0fcd64894bef"
 
   depends_on "gettext"
